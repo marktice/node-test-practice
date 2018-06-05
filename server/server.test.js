61145c779c45efb4cb9edcf787b8824d7527a4de
Mark Tice <marktice@protonmail.com>
2018-06-06 08:27:25 +1000

    Add describe to tests

--- server/server.test.js
+++ server/server.test.js
@@ -3,27 +3,34 @@ const expect = require('expect')
 
 const app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toMatchObject({
-        error: 'Page not found.'
-      });
-    })
-    .end(done)
-});
-
-it('should return my user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toContainEqual({
-        name: 'Mark',
-        age: 29,
-      });
-    })
-    .end(done)
-});
+describe('server', () => {
+  
+  describe('GET /', () => {
+    it('should return error response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toMatchObject({
+            error: 'Page not found.'
+          });
+        })
+        .end(done)
+    });
+  })
+ 
+  describe('GET /users', () => {
+    it('should return my user object', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toContainEqual({
+            name: 'Mark',
+            age: 29,
+          });
+        })
+        .end(done)
+    });
+  });
+})
